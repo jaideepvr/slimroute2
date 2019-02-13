c00f3521e37f1c21ad595fe53ad60050c75547ef
Jaideep Vinakota <jaideep@grandvinsoft.com>
2019-02-12 22:16:15 -0500

    Removed the extra $ sign

--- src/AnnotationRouteMapper.php
+++ src/AnnotationRouteMapper.php
@@ -89,7 +89,7 @@ class AnnotationRouteMapper {
      */
     private static function prepareRoutePath($prefix, $givenPath) {
         if ($givenPath[0] == '/') {
-            $path = $$givenPath;
+            $path = $givenPath;
         } else {
             $lastChar = substr($prefix, -1);
             
